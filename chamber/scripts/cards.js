af779e2ddfd524c2d2a6b3a9ca2d054f2731b1c7
raulgarcia99 <rgarciasamayoa@byupathway.edu>
2025-11-11 12:07:17 -0600

    cards created

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -1,14 +1,42 @@
-/*fetch("../data/members.json")
-    .then(response => response.json())
-    .then(data => {
+const fetchData = async() => {
+    try{
+        const response = await fetch("data/members.json");
+        const data = await response.json();
         console.log(data);
-    })
-    .catch(error => {
-        console.error("Sorry, something went wrong", error);
-    });*/
+        createCards(data);
+    } catch (error) {
+        console.error("Error fetching data", error);
+    }
+}
 
-fetch('../data/members.json')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
-  
+fetchData();
+
+function createCards(data) {
+    document.querySelector(".cards").innerHTML = "";
+    data.forEach(member => {
+        let card = document.createElement("section");
+        let img = document.createElement("img");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let url = document.createElement("a");
+
+        address.innerHTML = member.address;
+        phone.innerHTML = member.phone;
+        url.innerHTML = member.url;
+
+        card.setAttribute("class", "card");
+        img.setAttribute("src", member.logo);
+        img.setAttribute("alt", `${member.name} logo`);
+        img.setAttribute("width", "300px");
+        img.setAttribute("height","auto");
+        img.setAttribute("loading", "lazy");
+        url.setAttribute("href", member.url);
+
+        card.appendChild(img);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        document.querySelector(".cards").appendChild(card);
+    });
+}
